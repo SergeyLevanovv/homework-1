455121b221c40a91d380ebfe5297d10fc3e0df19
SergeyLevanovv <ser.lewanow@gmail.com>
2024-04-18 19:22:03 +0400

    add Task01.java

---
+++ src/Task01.java
@@ -0,0 +1,11 @@
+public class Task01 {
+    public static void main(String[] args) {
+                System.out.println("Задание №1");
+                int x = 21;
+                int y = 4;
+                int q = x / y;
+                int w = x % y;
+                System.out.println(x + " / " + y + " = " + q + " и " + w + " в остатке");
+                System.out.println("Квадратный корень x " + Math.sqrt(x) );
+            }
+        }
