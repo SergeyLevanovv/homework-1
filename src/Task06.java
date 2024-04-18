497d95b75e044b6e449feaa9067551656ea29940
SergeyLevanovv <ser.lewanow@gmail.com>
2024-04-18 20:43:45 +0400

    add Task05.java

---
+++ src/Task06.java
@@ -0,0 +1,13 @@
+public class Task06 {
+    public static void main(String[] args) {
+        final int W = 5;
+        final int H = 10;
+        System.out.println("Задание №6");
+        int s = W * H;
+        int p = (W + H) * 2;
+        System.out.println("square - " + s);
+        System.out.println("Perimeter - " + p);
+
+
+    }
+}
