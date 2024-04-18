db814b5e1659a8e02d73098ec831347e4f31ea00
SergeyLevanovv <ser.lewanow@gmail.com>
2024-04-18 21:10:20 +0400

    add Task08.java

---
+++ src/Task08.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Task08 {
+
+    public static final double V = 330.0;
+
+    public static void main(String[] args) {
+        System.out.println("Задание №8");
+        double t, s;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите временной промежуток между вспышкой " + "молнии и громом (секунд):");
+        t = sc.nextDouble();
+        s = t * V;
+        System.out.println("Расстояние до места удара молнии: " + s + " метров");
+
+    }
+}
