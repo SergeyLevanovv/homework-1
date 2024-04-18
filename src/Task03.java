986681bd916a4d74b834452fa2c2cb475b5bdc1b
SergeyLevanovv <ser.lewanow@gmail.com>
2024-04-18 19:57:38 +0400

    add Task03.java

---
+++ src/Task03.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Task03 {
+    public static void main(String[] args) {
+        System.out.println("Задание №3");
+        Scanner sc = new Scanner(System.in);
+        float n = Math.round(sc.nextFloat());
+        System.out.println("Result=" + n);
+    }
+}
